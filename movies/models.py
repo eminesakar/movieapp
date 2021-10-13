832a3508186a03c2df7a1649b35f3ffcf6f55284
“sadikturan” <sadikturan@gmail.com>
2021-10-13 12:41:16 +0300

    savechanges

--- movies/models.py
+++ movies/models.py
@@ -6,19 +6,10 @@ from django.db.models.fields import CharField
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
-class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(validators = [MinLengthValidator(20)])
-    image_name = models.CharField(max_length=50)
-    image_cover = models.CharField(max_length=50)
-    date = models.DateField()
-    slug = models.SlugField(unique=True,db_index=True)
-    budget = models.DecimalField(max_digits=19,decimal_places=2)
-    language = models.CharField(max_length=100)
-
 class Contact(models.Model):
     address = models.CharField(max_length=200)
     email = models.EmailField()
+
 
 class Person(models.Model):
 
@@ -41,3 +32,21 @@ class Person(models.Model):
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1, choices=genders)
     duty_type = models.CharField(max_length=1, choices=duty_types)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True, blank=True)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(validators = [MinLengthValidator(20)])
+    image_name = models.CharField(max_length=50)
+    image_cover = models.CharField(max_length=50)
+    date = models.DateField()
+    slug = models.SlugField(unique=True,db_index=True)
+    budget = models.DecimalField(max_digits=19,decimal_places=2)
+    language = models.CharField(max_length=100)
+    people = models.ManyToManyField(Person)
+    genres = models.ManyToManyField(Genre)
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
