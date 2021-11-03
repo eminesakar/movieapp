9b5b9bd38124ada1029e0004def0e636045fdbec
“sadikturan” <sadikturan@gmail.com>
2021-11-03 11:25:13 +0300

    savechanges

--- movies/models.py
+++ movies/models.py
@@ -75,6 +75,13 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(),
+    text = models.TextField(max_length=500)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="comments")
+
+
 class Video(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
