53657d47cbe697b395417fbcc426216a24d6b1b8
“sadikturan” <sadikturan@gmail.com>
2021-11-04 14:56:16 +0300

    savechanges

--- movieapp/settings.py
+++ movieapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'movies',
     'django.contrib.admin',
     'django.contrib.auth',
--- movieapp/urls.py
+++ movieapp/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include("movies.urls")),
+    path('account/', include("account.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
