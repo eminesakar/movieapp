9b77f2fb90b65b5dcd56a5ec83b615755ab247da
“sadikturan” <sadikturan@gmail.com>
2021-11-05 22:32:15 +0300

    savechanges

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-from account.forms import LoginForm
+from account.forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import User
 
 def login_request(request):
@@ -12,12 +12,16 @@ def login_request(request):
         if form.is_valid():
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
-           
+            remember_me = form.cleaned_data.get("remember_me")
+
             username = User.objects.get(email=email).username
             user = authenticate(username=username, password=password)
 
             if user is not None:
                 login(request, user)
+                if not remember_me:
+                    request.session.set_expiry(0)
+                    request.session.modified=True
                 return redirect("home_page")
             else:
                 form.add_error(None, "email ya da parola yanlış.")
@@ -31,7 +35,25 @@ def login_request(request):
     return render(request, 'account/login.html',{'form':form})
 
 def register_request(request):
-    return render(request, 'account/register.html')
+    if request.user.is_authenticated:
+        return redirect("home_page")
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password = password)
+            login(request,user)
+            return redirect("home_page")
+        else:
+            form.add_error(None, "formu eksiksiz doldurmalısınız.")
+            return render(request, 'account/register.html', {"form":form})
+
+    form = CreateUserForm()
+    return render(request, 'account/register.html', {"form":form})
 
 def change_password(request):
     return render(request, 'account/change_password.html')
