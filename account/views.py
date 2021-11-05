3e6ffe8f2e7f1c3a1739ef17c3aad75b3e394764
“sadikturan” <sadikturan@gmail.com>
2021-11-05 23:37:52 +0300

    savechanges

--- account/views.py
+++ account/views.py
@@ -42,8 +42,8 @@ def register_request(request):
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
+            user = form.save()
+            username = user.username
             password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password = password)
             login(request,user)
